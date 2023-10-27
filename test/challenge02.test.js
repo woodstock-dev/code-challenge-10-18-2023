33548605aa1668df7323701f0b59b14b342eec4e
Andrew Klapper <ak475826@gmail.com>
2023-10-26 20:59:55 -0400

    revised test cases

--- test/challenge02.test.js
+++ test/challenge02.test.js
@@ -1,16 +1,31 @@
 import generatePassword from "../src/challenge02";
 
 describe("Challenge 2", () => {
-  test("Generate Password", () => {
-    for (let i = 0; i < 100; i++) {
-      if (i < 8) expect(() => generatePassword(i)).toThrow();
-      else {
-        expect(generatePassword(i)).toHaveLength(i);
-        expect(generatePassword(i)[0]).toMatch(/[A-Z]/);
-        for (let j = 1; j < generatePassword(i).length; j++) {
-          expect(generatePassword(i)[j]).toMatch(/^[\x22-\x7E]*$/);
-        }
+  test("Throw error for password", () => {
+    for (let i = 0; i < 8; i++) {
+      expect(() => generatePassword(i)).toThrow();
+    }
+  })
+      
+  test('Password length', () => {
+    for (let i = 8; i < 100; i++) {
+      expect(generatePassword(i)).toHaveLength(i);
+
+    }   
+  })
+
+  test('First letter', () => {
+    for (let i = 8; i < 100; i++) {
+      expect(generatePassword(i)[0]).toMatch(/[A-Z]/);
+
+    }
+  })
+
+  test('Remaning letters', () => {
+    for (let i = 8; i < 100; i++) {
+      for (let j = 1; j < generatePassword(i).length; j++) {
+        expect(generatePassword(i)[j]).toMatch(/^[\x22-\x7E]*$/);
       }
     }
-  });
+  })
 });
